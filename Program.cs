e2acde31188094ace9d2f476634f6320de56e86e
ronsiv8 <ronsivan81@gmail.com>
2022-02-28 09:08:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,6 +25,43 @@ namespace ConsoleApp34
             }
             Console.WriteLine("Q:" + CountOfQ);
             Console.WriteLine("E:" + CountOfE);
+            // question 2
+            string[] Card = new string[16];
+            Random rand = new Random();
+            for (int i = 0; i < Card.Length; i++)
+            {
+                int gotNum = rand.Next(1, 4);
+                string sym = "";
+                switch (gotNum) {
+                    case 1:
+                        sym = "1";
+                        break;
+                    case 2:
+                        sym = "2";
+                        break;
+                    case 3:
+                        sym = "x";
+                        break;
+                }
+                Card[i] = sym;
+            }
+            for (int i = 0; i < Card.Length; i++)
+            {
+                Console.Write(" " + Card[i] + " ");
+            }
+            Console.WriteLine("");
+            string[] userCard = new string[16];
+            for (int i = 0; i < Card.Length; i++)
+            {
+                userCard[i] = Console.ReadLine();
+            }
+            // ok now check how many correct
+            int AmtCorrect = 0;
+            for (int i = 0; i < Card.Length; i++)
+            {
+                if (Card[i] == userCard[i]) AmtCorrect++;
+            }
+            Console.WriteLine("Correct gusses:" + AmtCorrect);
         }
     }
 }
